2101a5a2bf7a5c04df8100fd2e71db85555cea18
bngone <88200741+bngone@users.noreply.github.com>
2021-08-10 18:46:58 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// telegram token
+var token = '1729110652:AAG52j0IoFDDDltDI4oUvh1aQ8smDSMK2Zc'
+// firebase config
+var config = {
+    apiKey: "AIzaSyAKbt0eqyKbZm1fTvD_RYsBrVbnf4OuqRA",
+    authDomain: "airdrob-14881.firebaseapp.com",
+    databaseURL: "https://airdrob-14881-default-rtdb.firebaseio.com",
+    projectId: "airdrob-14881",
+    storageBucket: "airdrob-14881.appspot.com",
+    messagingSenderId: "403108780180"
+};
+
+exports.token = token;
+exports.firebase_config = config;
